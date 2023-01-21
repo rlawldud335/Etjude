5f0212dae6f08f921e37750a0a2187724afe4983
dhlim07 <dhlim07@naver.com>
2023-01-21 17:22:45 +0900

    feat: setup

---
+++ FE/client/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "@vue/airbnb",
+    "prettier",
+  ],
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+  },
+  rules: {
+    "no-console":
+      process.env.NODE_ENV === "production"
+        ? "warn"
+        : "off",
+    "no-debugger":
+      process.env.NODE_ENV === "production"
+        ? "warn"
+        : "off",
+  },
+};
