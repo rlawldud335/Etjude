34beba1553c1417300dcf4e84c4c2ed5798b0c38
김지영 <rlawldud335@naver.com>
2023-01-25 18:06:26 +0900

    feat : reset.scss 전역 적용

--- FE/vue.config.js
+++ FE/vue.config.js
@@ -2,4 +2,14 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import "@/assets/scss/common.scss";
+          @import "@/assets/scss/reset.scss";
+        `
+      }
+    }
+  }
 });
