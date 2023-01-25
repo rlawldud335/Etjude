34537ed3b1517f9dc3c112e45d645aac0c6a2460
unknown <dhlim07@naver.com>
2023-01-25 18:00:41 +0900

    feat : update-header

--- FE/src/App.vue
+++ FE/src/App.vue
@@ -1,30 +1,66 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="header">
+    <div class="header__logo">
+      <span>Logo</span>
+      <div class="header__nav">
+        <router-link to="/" class="header__nav-item">검색</router-link>
+        <router-link to="/about" class="header__nav-item">카테고리</router-link>
+      </div>
+    </div>
+    <div>
+      <button class="header__login-button">Login</button>
+    </div>
   </nav>
   <router-view />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url(//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css);
+* {
+  font-family: "Spoqa Han Sans Neo", "sans-serif";
 }
 
+$bana-pink: #ff5775;
+
 nav {
-  padding: 30px;
+  padding: 16px;
 
   a {
     font-weight: bold;
     color: #2c3e50;
+    text-decoration: none;
 
     &.router-link-exact-active {
       color: #42b983;
     }
   }
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+}
+
+.header__logo {
+  display: flex;
+  color: $bana-pink;
+  font-weight: 1000;
+}
+
+.header__nav {
+  display: flex;
+}
+
+.header__nav-item {
+  color: black;
+  margin: 3px;
+}
+
+.header__login-button {
+  border: none;
+  width: 87px;
+  height: 32px;
+  background: #ff5775;
+  border-radius: 4px;
 }
 </style>
