a1e6d2491b8a1b024e604cb8075adb11be9872e9
김지영 <rlawldud335@naver.com>
2023-01-26 09:24:40 +0900

    feat : MainView.vue 파일 생성

--- FE/src/router/index.js
+++ FE/src/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+// import HomeView from '../views/HomeView.vue';
+import MainView from '../views/MainView.vue';
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView,
+    component: MainView,
   },
   {
     path: '/about',
---
+++ FE/src/views/MainView.vue
@@ -0,0 +1,16 @@
+<template lang="">
+    <div class="main-page">
+        메인 페이지
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+    .main-page {
+        background-color: pink;
+    }
+</style>
