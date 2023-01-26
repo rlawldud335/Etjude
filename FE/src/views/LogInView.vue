5696894d52c03353cbda3c16c32770203976140d
임두현 <dhlim07@naver.com>
2023-01-26 14:22:34 +0900

    feat : updatelogin page

--- FE/src/views/LogInView.vue
+++ FE/src/views/LogInView.vue
@@ -1,9 +1,36 @@
 <template>
   <h2>로그인</h2>
-  <button>google로 로그인하기</button>
+  <button @click="login">
+    google로 로그인하기
+  </button>
 </template>
 
 <script>
-export default {};
+import axios from 'axios'
+
+const ARI_URL = ''
+
+export default {
+  name: "LoginView",
+  data() {
+    return {};
+  },
+  computed: {
+    login() {
+      axios({
+        method: 'get',
+        url: '{API_URL}',
+				params: {},
+				data: {},
+      });
+				.then((response) => {
+          console.log(response)
+        })
+				.catch((error) => {
+					console.log(error)
+				})
+    },
+  },
+};
 </script>
 <style lang="scss" scoped></style>
