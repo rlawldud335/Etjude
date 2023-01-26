e864d418b0ad5d3cf80ddea460c113185dee4f39
임두현 <dhlim07@naver.com>
2023-01-26 13:32:36 +0900

    feat : add login page

--- FE/src/router/index.js
+++ FE/src/router/index.js
@@ -1,19 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import {
+  createRouter,
+  createWebHistory,
+} from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import LogInView from "../views/LogInView.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
+    path: "/",
+    name: "home",
     component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
+    path: "/login",
+    name: "LogInView",
+    component: LogInView,
+  },
+  {
+    path: "/about",
+    name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    component: () =>
+      import(
+        /* webpackChunkName: "about" */ "../views/AboutView.vue"
+      ),
   },
 ];
 
---
+++ FE/src/views/LogInView.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2>로그인</h2>
+  <button>google로 로그인하기</button>
+</template>
+
+<script>
+export default {};
+</script>
+<style lang="scss" scoped></style>
