02b1d49bf101bba1c7a35b1f4e6846412e2f6464
박병우 <bottler3123@naver.com>
2023-01-30 18:05:12 +0900

    feat : modal

--- FE/src/components/Share/SharingDtail.vue
+++ FE/src/components/Share/SharingDtail.vue
@@ -1,8 +1,25 @@
 <template lang="">
   <div>
-    <vue-final-modal v-model="showModal"> test modal </vue-final-modal>
-    <button @click="showModal = true">open modal</button>
+    <vue-final-modal v-model="showModal">
+      <span class="modal__title">Hello, vue-final-modal</span>
+    </vue-final-modal>
+    <button @click="showModal = true">Open modal</button>
   </div>
 </template>
-<script></script>
-<style lang=""></style>
+<script>
+import vfmPlugin from "vue-final-modal";
+
+export default {
+  data: () => ({
+    Object: vfmPlugin,
+    showModal: false,
+  }),
+  methods: {},
+};
+</script>
+<style scoped>
+.modal__title {
+  font-size: 1.5rem;
+  font-weight: 700;
+}
+</style>
--- FE/src/main.js
+++ FE/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from "vue";
+import { vfmPlugin } from "vue-final-modal";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
 createApp(App).use(store).use(router).mount("#app");
+
+App.use(vfmPlugin);
