44dea774a94a3df6f71b2dcd897fa965dbb9a1f0
김지영 <rlawldud335@naver.com>
2023-01-31 12:36:46 +0900

    fix : 헤더풋터 있는 페이지와 없는페이지 나누기

---
+++ FE/src/views/NavFooterView.vue
@@ -0,0 +1,16 @@
+<template>
+    <MainNav/>
+    <router-view/>
+    <MainFooter/>
+</template>
+<script>
+import MainNav from '@/components/common/MainNav.vue';
+import MainFooter from '@/components/common/MainFooter.vue';
+
+export default {
+    components:{
+        MainNav,
+        MainFooter
+    }
+}
+</script>
