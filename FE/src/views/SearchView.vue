6af05893db9664fb406420744c3db8d717a7a1dd
임두현 <dhlim07@naver.com>
2023-02-09 15:52:46 +0900

    feat : updata search page layout

--- FE/src/views/SearchView.vue
+++ FE/src/views/SearchView.vue
@@ -9,18 +9,68 @@
           @blur="blurInput"
         />
       </div>
-      <div class="search__tag">
-        <button class="search__tag-button"># 드라마</button>
-        <button class="search__tag-button"># 영화</button>
-        <button class="search__tag-button"># 연극</button>
-        <button class="search__tag-button"># 기타</button>
+      <div class="search__body main__1136width">
+        <div class="search__categoty-tab">
+          <button
+            :class="[
+              state.category === 'all' ? 'search__category--active' : 'search__category--unactive',
+            ]"
+            @click="state.category = 'all'"
+          >
+            전체
+          </button>
+          <button
+            :class="[
+              state.category === 'movie'
+                ? 'search__category--active'
+                : 'search__category--unactive',
+            ]"
+            @click="state.category = 'movie'"
+          >
+            영화
+          </button>
+          <button
+            :class="[
+              state.category === 'drama'
+                ? 'search__category--active'
+                : 'search__category--unactive',
+            ]"
+            @click="state.category = 'drama'"
+          >
+            드라마
+          </button>
+          <button
+            :class="[
+              state.category === 'play' ? 'search__category--active' : 'search__category--unactive',
+            ]"
+            @click="state.category = 'play'"
+          >
+            연극
+          </button>
+        </div>
+        <div class="search__result-section">
+          <div class="search__toggle-tab">
+            <button @click="state.menu = 'work'">작품</button>
+            <button @click="state.menu = 'story'">스토리</button>
+            <div
+              :class="[
+                {
+                  'search__bar--work': state.menu === 'work',
+                  'search__bar--story': state.menu === 'story',
+                },
+                'active-bar',
+              ]"
+            ></div>
+          </div>
+          <div class="search__result"></div>
+        </div>
       </div>
     </div>
   </div>
   <SearchResult v-if="inputText" :keyword="inputText"></SearchResult>
 </template>
 <script>
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 import { useRouter, useRoute } from "vue-router";
 import SearchResult from "@/components/search/SearchResult.vue";
 
@@ -44,10 +94,15 @@ export default {
         router.push({ name: "search" });
       }
     };
+    const state = reactive({
+      category: "all",
+      menu: "work",
+    });
     return {
       inputText,
       inputKeyword,
       blurInput,
+      state,
     };
   },
   beforeRouteUpdate(to, from, next) {
@@ -72,7 +127,7 @@ export default {
   align-items: center;
 }
 .search__input {
-  background-color: #ffeff2;
+  background-color: $soft-bana-pink;
   padding: 15px 40px;
   border-radius: 30px;
   display: flex;
@@ -85,25 +140,72 @@ export default {
   margin: 5px 0px;
   font-size: 14px;
 }
-.search__glass-icon {
-  margin-left: 10px;
-  cursor: pointer;
+
+.search__body {
+  display: flex;
+  flex-direction: row;
+  margin-top: 50px;
 }
 
-.search__tag {
-  width: 600px;
-  margin: 10px 0px;
+.search__categoty-tab {
+  display: flex;
+  flex-direction: column;
+  width: 20%;
+  button {
+    font-weight: 500;
+    border-radius: 5px;
+    padding: 10px;
+    width: 80%;
+    margin: 0px 15px;
+    border: none;
+    cursor: pointer;
+  }
+  .search__category--unactive {
+    background-color: $white;
+  }
+  .search__category--active {
+    background-color: $soft-bana-pink;
+    color: $bana-pink;
+  }
+  .search__category--unactive:hover {
+    background-color: $efefe-gray;
+    color: black;
+  }
 }
 
-.search__tag-button {
-  border: none;
-  color: white;
-  font-size: 14px;
-  background-color: $bana-pink;
-  width: 85px;
-  height: 32px;
-  border-radius: 20px;
-  margin-right: 10px;
-  cursor: pointer;
+.search__result-section {
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  margin-left: 50px;
+}
+
+.search__toggle-tab {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  position: relative;
+  button {
+    background-color: white;
+    border: none;
+    width: 130px;
+    padding: 10px 5px 10px 5px;
+  }
+}
+
+.active-bar {
+  position: absolute;
+  height: 6px;
+  transition: 1s;
+  top: 35px;
+  width: 130px;
+  background: #ff5775;
+  transition: 0.3s ease;
+}
+.search__bar--work {
+  left: 0px;
+}
+.search__bar--story {
+  left: 130px;
 }
 </style>
