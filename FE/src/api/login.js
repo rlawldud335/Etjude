e95bc056d15288a8e57e73f43d69e7cd6fd6e2f0
박병우 <bottler3123@naver.com>
2023-02-12 02:28:19 +0900

    feat : 스토리 선택 후 api 적용

--- FE/src/api/login.js
+++ FE/src/api/login.js
@@ -1,4 +1,5 @@
 import firebaseConfig from "@/firebaseConfig";
+// eslint-disable-next-line import/extensions
 import { getAuth, signInWithPopup, signOut, GoogleAuthProvider } from "firebase/auth";
 import { apiInstance } from "@/api/index";
 
@@ -11,38 +12,38 @@ const auth = getAuth();
 const provider = new GoogleAuthProvider();
 
 async function login(user, success, fail) {
-    console.log("# POST : 회원가입 및 로그인");
-    await api.post(`/user/login`, user).then(success).catch(fail);
+  console.log("# POST : 회원가입 및 로그인");
+  await api.post(`/user/login`, user).then(success).catch(fail);
 }
 
 export function handleSignInGoogle() {
-    signInWithPopup(auth, provider)
-        .then((result) => {
-            this.user = {
-                "userId" : result.user.uid,
-                "email" : result.user.email,
-                "nickName" : result.user.displayName,
-                "picture" : result.user.photoURL,
-                "roleType" : "USER",
-            }
-            login(this.user);
-            this.$store.dispatch("login", result.user.accessToken);
-            this.$router.push({ name: 'main' });
-        })
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      this.user = {
+        userId: result.user.uid,
+        email: result.user.email,
+        nickName: result.user.displayName,
+        picture: result.user.photoURL,
+        roleType: "USER",
+      };
+      login(this.user);
+      this.$store.dispatch("login", result.user.accessToken);
+      this.$router.push({ name: "main" });
+    })
     .catch((error) => {
-        console.log(error);
+      console.log(error);
     });
 }
 
 export function handleSignOut() {
-    signOut(auth)
-        .then(() => {
-            this.user = "";
-            this.$store.dispatch("logout");
-            this.$router.push({ name: 'main' })
-        })
+  signOut(auth)
+    .then(() => {
+      this.user = "";
+      this.$store.dispatch("logout");
+      this.$router.push({ name: "main" });
+    })
     .catch((error) => {
-        console.log(error);
+      console.log(error);
     });
 }
 
