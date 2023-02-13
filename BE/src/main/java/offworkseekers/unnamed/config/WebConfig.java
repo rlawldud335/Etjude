ca7ca2eb5a83e7778de043e5dad9bb81379e30ae
Taekyun Kim <taekyun.contact@gmail.com>
2023-02-13 21:57:27 +0900

    fix : cors config

--- BE/src/main/java/offworkseekers/unnamed/config/WebConfig.java
+++ BE/src/main/java/offworkseekers/unnamed/config/WebConfig.java
@@ -1,6 +1,7 @@
 package offworkseekers.unnamed.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,6 +10,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("*");
+                .allowedOrigins("*")
+                .allowedMethods(
+                HttpMethod.GET.name(),
+                HttpMethod.HEAD.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.DELETE.name());
     }
 }
