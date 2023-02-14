422b57aa14d91cdf6aec725cce8657b1d0a96c8b
임두현 <dhlim07@naver.com>
2023-02-15 00:23:54 +0900

    fix : fix merge miss

--- FE/src/views/MainView.vue
+++ FE/src/views/MainView.vue
@@ -7,11 +7,9 @@
     <div class="main__1136width">
       <SearchSession />
     </div>
-
-    <div class="content__1136width">
+    <div class="main__studio-list content__1136width" v-show="showStudio">
       <StudioCard></StudioCard>
     </div>
-
     <div class="content__1136width">
       <StoryCard></StoryCard>
     </div>
@@ -33,7 +31,8 @@ import StoryCard from "@/components/main/StoryCard.vue";
 import PieceCard from "@/components/main/PieceCard.vue";
 import ShortcutFlim from "@/components/main/ShortcutFlim.vue";
 import SearchSession from "@/components/main/SearchSession.vue";
-// import { onMounted } from "vue";
+import { computed, ref, watch } from "vue";
+import { useStore } from "vuex";
 
 export default {
   name: "MainView",
@@ -44,9 +43,22 @@ export default {
     PieceCard,
     ShortcutFlim,
     SearchSession,
-    // inputDropdown,
   },
-  setup() {},
+  setup() {
+    const store = useStore();
+    const user = computed(() => store.state.user);
+    const myStudioCount = computed(() => store.state.user.myStudioCount);
+    const showStudio = ref(myStudioCount.value > 0);
+    watch(myStudioCount, (newCount) => {
+      console.log("new", newCount);
+      showStudio.value = newCount > 0;
+    });
+    return {
+      user,
+      showStudio,
+      myStudioCount,
+    };
+  },
 };
 </script>
 <style lang="scss">
