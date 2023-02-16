1e6b2a48b884b5210b1b109402f3f86bdfa611fe
sujung <hsj8518@gmail.com>
2023-02-16 20:41:05 +0900

    fix: hidden.yml 추가

--- BE/src/main/java/offworkseekers/unnamed/UnnamedApplication.java
+++ BE/src/main/java/offworkseekers/unnamed/UnnamedApplication.java
@@ -4,14 +4,21 @@ package offworkseekers.unnamed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling // 스케줄러 사용을 위해 추가
 @SpringBootApplication
 public class UnnamedApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "classpath:hidden.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(UnnamedApplication.class, args);
+		new SpringApplicationBuilder(UnnamedApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
 
 }
