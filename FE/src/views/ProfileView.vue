d907b54ea10db04f63ebb6aaa0c4952e432580d1
임두현 <dhlim07@naver.com>
2023-02-20 13:50:53 +0900

    feat : Add Profileview

---
+++ FE/src/views/ProfileView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="profile">
+    <div class="profile__thumbnail-frame">
+      <img src="" alt="" />
+    </div>
+    <div class="profile__nickname">
+      <span></span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "ProfileView",
+  components: {},
+  setup() {
+    return {};
+  },
+};
+</script>
+<style lang="scss" scoped></style>
