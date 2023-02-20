5173bc3d7f8bdc02e6e98154aed99d3e222c4e55
임두현 <dhlim07@naver.com>
2023-02-20 17:51:49 +0900

    feat : Add profile components

---
+++ FE/src/components/profile/ProfileCommentList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="profile-comments"></div>
+</template>
+
+<script>
+export default {
+  name: "ProfileCommentList",
+  components: {},
+  props: {},
+  setup() {
+    return {};
+  },
+};
+</script>
+<style lang="scss" scoped></style>
---
+++ FE/src/components/profile/ProfileFilmList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="profile-films"></div>
+</template>
+
+<script>
+export default {
+  name: "ProfileFilmList",
+  components: {},
+  props: {},
+  setup() {
+    return {};
+  },
+};
+</script>
+<style lang="scss" scoped></style>
