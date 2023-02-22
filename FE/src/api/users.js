66ccac7230897af067f5fe9a96ea217a42ecbf24
박병우 <bottler3123@naver.com>
2023-02-22 12:51:25 +0900

    feat : 마이페이지 나의 스튜디오 제작

--- FE/src/api/users.js
+++ FE/src/api/users.js
@@ -18,8 +18,16 @@ async function getMyFilm(user, success, fail) {
   await api.post(`/mypage/film`, user).then(success).catch(fail);
 }
 
+async function getMyArticle(user, success, fail) {
+  await api.post(`/mypage/articles`, user).then(success).catch(fail);
+}
+
+async function getMyComment(user, success, fail) {
+  await api.post(`/mypage/comments`, user).then(success).catch(fail);
+}
+
 async function getUserSearch(email, success, fail) {
   await api.post(`/studio/search`, email).then(success).catch(fail);
 }
 
-export { getMyPage, getMyLike, getMyStudio, getMyFilm, getUserSearch };
+export { getMyPage, getMyLike, getMyStudio, getMyFilm, getMyArticle, getMyComment, getUserSearch };
